1db075cc41a14067d966af0b6af38c5e722c703e
victorychan1117 <victorychan1117@gmail.com>
2025-11-09 18:42:23 +0900

    test: 테스트는 문서이다.

--- src/test/java/com/example/testcode/unit/beverage/CafeKioskTest.java
+++ src/test/java/com/example/testcode/unit/beverage/CafeKioskTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.example.testcode.unit.Latte;
 import com.example.testcode.unit.order.Order;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CafeKioskTest {
@@ -25,6 +26,7 @@ class CafeKioskTest {
     System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
   }
 
+  @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
   @Test
   void add_auto_test() {
     // ✅ 자동 테스트 (JUnit이 직접 검증)
@@ -118,5 +120,24 @@ class CafeKioskTest {
     assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2025, 1, 17, 9, 59)))
         .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+  }
+
+
+  @DisplayName("주문 목록에 담긴 상품들의 총 그맹긍ㄹ 계산할 수 있다.")
+  @Test
+  void calculateTotalPrice(){
+      // given
+      CafeKiosk cafeKiosk = new CafeKiosk();
+      Americano americano = new Americano();
+      Latte latte = new Latte();
+
+      cafeKiosk.add(americano);
+      cafeKiosk.add(latte);
+
+      // when
+      int totalPrice = cafeKiosk.calculateTotalPrice();
+
+      // then
+    assertThat(totalPrice).isEqualTo(8500);
   }
 }
